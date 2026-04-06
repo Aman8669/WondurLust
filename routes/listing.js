23b359e1fe493a3debaf89741bcb2b41ec985f1c
Aman Naikwade <amannaikwade09@gmail.com>
2026-04-06 17:07:03 +0530

    Configure Cloudinary for image upload

--- routes/listing.js
+++ routes/listing.js
@@ -3,15 +3,20 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 // INDEX + CREATE
 router.route("/")
     .get(wrapAsync(listingController.index))
     .post(
         isLoggedIn,
+        upload.single('listing[image]'),
         validateListing,
         wrapAsync(listingController.createListing)
     );
+
 
 // NEW
 router.get("/new", isLoggedIn, listingController.listingRender);
